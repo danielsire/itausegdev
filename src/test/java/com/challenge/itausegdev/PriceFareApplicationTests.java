9b0acb35caf03ba856d985169dae3ab839043162
dsire <sirehorta@gmail.com>
2024-03-16 11:29:12 -0300

    fixes

--- src/test/java/com/challenge/itausegdev/PriceFareApplicationTests.java
+++ src/test/java/com/challenge/itausegdev/PriceFareApplicationTests.java
@@ -2,8 +2,10 @@ package com.challenge.itausegdev;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("tests")
 class PriceFareApplicationTests {
 
 	@Test
